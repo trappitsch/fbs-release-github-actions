4c50aed4b96b2b933d20e2253252e215ff1fa3e8
Reto Trappitsch <reto@galactic-forensics.space>
2020-07-14 18:33:36 -0700

    Tutorial App, bump2version, requirements.txt added

---
+++ src/main/python/main.py
@@ -0,0 +1,31 @@
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
+
+import requests
+import sys
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        text = QLabel()
+        text.setWordWrap(True)
+        button = QPushButton('Next quote >')
+        button.clicked.connect(lambda: text.setText(_get_quote()))
+        layout = QVBoxLayout()
+        layout.addWidget(text)
+        layout.addWidget(button)
+        layout.setAlignment(button, Qt.AlignHCenter)
+        self.setLayout(layout)
+
+def _get_quote():
+    return requests.get('https://build-system.fman.io/quote').text
+
+if __name__ == '__main__':
+    appctxt = ApplicationContext()
+    stylesheet = appctxt.get_resource('styles.qss')
+    appctxt.app.setStyleSheet(open(stylesheet).read())
+    window = MainWindow()
+    window.show()
+    exit_code = appctxt.app.exec_()
+    sys.exit(exit_code)
